20cb6176c5015c9c8bfcadc118ebb180ece9e66c
Ankit Chaurasia <ac13793@gmail.com>
2017-08-17 23:50:55 +0530

    React Components

--- The Net Ninja Tutorial/src/app/index.js
+++ The Net Ninja Tutorial/src/app/index.js
@@ -1,1 +1,24 @@
-alert("It works!!");
+var React = require('react');
+var ReactDOM =require('react-dom');
+
+// Create component
+var ToDoComponent = React.createClass({
+    render: function(){
+        return(
+            <h1>Hello</h1>
+        );
+    }
+});
+
+// Put component into html page
+ReactDOM.render(<ToDoComponent/>, document.getElementById('todo-wrapper'));
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Ankit" />;
+ReactDOM.render(
+  element,
+  document.getElementById('myName')
+);
