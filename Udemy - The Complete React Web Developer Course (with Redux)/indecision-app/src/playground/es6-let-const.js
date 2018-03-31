51d862ff88b7f25bb69423d5b9c1728ed4d26389
Ankit Chaurasia <ac13793@gmail.com>
2018-03-31 16:28:57 +0530

    ES6 Aside const and let

---
+++ Udemy - The Complete React Web Developer Course (with Redux)/indecision-app/src/playground/es6-let-const.js
@@ -0,0 +1,10 @@
+var name = "Ankit";
+var name = "Chaurasia";
+console.log(name);
+let name2 = "Ankit2";
+name2 = "Chaurasia2"; // This line will give error
+console.log(name2);
+
+const nameConst  = "Ankit Const";
+// const nameConst  = "New Const";
+console.log('Const name: ' + nameConst);
