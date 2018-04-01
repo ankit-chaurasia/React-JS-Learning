274629b046cb726c8474c6bab404fb6014c3641b
Ankit Chaurasia <ac13793@gmail.com>
2018-04-01 22:26:54 +0530

    ES6 Classes Part II

--- Udemy - The Complete React Web Developer Course (with Redux)/indecision-app/src/playground/es6-classes-1.js
+++ Udemy - The Complete React Web Developer Course (with Redux)/indecision-app/src/playground/es6-classes-1.js
@@ -13,5 +13,43 @@ class Person {
     }
 }
 
+class Student extends Person {
+    constructor(name, age, major = 'Information Technology') {
+        super(name, age); // Before this parent constructor should be called
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescripton() {
+        let descripton = super.getDescripton();
+        if (this.hasMajor()) {
+            descripton += ` Their major is ${this.major}`;
+        }
+        return descripton;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if(this.homeLocation){
+            greeting += ` I am visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
 const me = new Person('Ankit Chaurasia', 25);
 console.log(me.getDescripton());
+
+const other = new Student('Student', 22, 'Computer Science');
+console.log(other.getDescripton());
+
+const someOne = new Traveler("Traveler", 30, 'Hyderebad');
+console.log(someOne.getGreeting());
