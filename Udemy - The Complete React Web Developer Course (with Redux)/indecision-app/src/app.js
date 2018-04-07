e7a59185fff5c7b25295e146797742c6a5970832
Ankit Chaurasia <ac13793@gmail.com>
2018-04-07 10:41:27 +0530

    Default Exports

--- Udemy - The Complete React Web Developer Course (with Redux)/indecision-app/src/app.js
+++ Udemy - The Complete React Web Developer Course (with Redux)/indecision-app/src/app.js
@@ -1,6 +1,7 @@
 // import './utils.js';
-import { square, add } from './utils.js'
+import subtract, { square, add } from './utils.js'
 
 console.log('app.js is running');
 console.log(square(4));
 console.log(add(4,5))
+console.log(subtract(4,5))
