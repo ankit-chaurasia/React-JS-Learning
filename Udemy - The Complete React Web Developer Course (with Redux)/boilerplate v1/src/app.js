41d37aadcd6d9fd184d76f4b74151f0773ffd3a8
Ankit Chaurasia <ac13793@gmail.com>
2018-04-14 11:03:14 +0530

    Setting Up Expensify

---
+++ Udemy - The Complete React Web Developer Course (with Redux)/boilerplate v1/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boler plate</p>, document.getElementById('app'));
---
+++ Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/app.js", // Entry point for webpack
+    output: {
+        path: path.join(__dirname, 'public'), // absolute path where you want to output your file
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+}
