f768f075413bb057c9f3a7d6fc8f3f963caec15d
Ankit Chaurasia <ac13793@gmail.com>
2018-04-15 09:31:23 +0530

    ES6 Array Destructuring

--- Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/playground/destructuring.js
+++ Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/playground/destructuring.js
@@ -1,3 +1,6 @@
+/**
+ * Object Destructuring
+ */
 const person = {
   name: "Ankit",
   age: 26,
@@ -6,16 +9,25 @@ const person = {
     temp: 92
   }
 };
-
 // const name = person.name;
 // const age = person.age;
-
 // ES6 destructuring
 // Renaming name to firstName and setting default value
 const { name: firstName = "defalt name", age } = person;
-
 console.log(`${firstName} is ${age}`);
-
 const { city, temp: temprature } = person.location;
-
 console.log(`It's ${temprature} in ${city}`);
+
+/**
+ * Array Destructuring
+ */
+const address = [
+  "1299 S Juniper Street",
+  "Philadelphia",
+  "Pennsylvania",
+  "19147"
+];
+// Matching by position
+const [, city, state = "New York"] = address;
+
+console.log(` You are in ${city} ${state}`);
