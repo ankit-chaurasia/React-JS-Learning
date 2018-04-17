676b954809dd921e5e6640361cbd6c06131f143f
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-04-17 10:55:22 +0530

    Dropdown for Picking SortBy

--- Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/app.js
+++ Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/app.js
@@ -12,8 +12,9 @@ import "./styles/styles.scss";
 const store = configureStore();
 
 store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill" }));
-store.dispatch(setTextFilter("bill"));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
+// store.dispatch(setTextFilter("bill"));
 const state = store.getState();
 const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
 console.log(visibleExpenses);
