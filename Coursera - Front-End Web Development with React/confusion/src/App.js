c78d0cf0cc1da3adf9caec0683a8cc82195fd785
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-06-26 12:16:29 +0530

    Updated css for navheader

--- Coursera - Front-End Web Development with React/confusion/src/App.js
+++ Coursera - Front-End Web Development with React/confusion/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/" className='navBrand'>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
       </div>
