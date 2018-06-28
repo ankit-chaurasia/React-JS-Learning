fdb59fc11ea5d96860f26f21b0d0f17f798c9b0d
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-06-28 16:39:01 +0530

    Single Page Applications Part 2

--- Coursera - Front-End Web Development with React/confusion/src/components/DishDetails.js
+++ Coursera - Front-End Web Development with React/confusion/src/components/DishDetails.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {
-    Card,
-    CardImg,
-    CardText,
-    CardBody,
-    CardTitle
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-const renderDish = (dish) => {
+const RenderDish = (props) => {
+    const dish = props.dish;
     return (
         <div className="col-12 col-md-5 m-1">
             <Card>
@@ -21,18 +20,19 @@ const renderDish = (dish) => {
     );
 };
 
-const renderComments = (comments) => {
-    if(comments) {
+const RenderComments = (props) => {
+    const comments = props.comments;
+    if (comments) {
         return (
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
                     {
-                        comments.map((comment)=>{
+                        comments.map((comment) => {
                             return (
                                 <li key={comment.id}>
                                     <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                                 </li>
                             )
                         })
@@ -45,14 +45,27 @@ const renderComments = (comments) => {
     }
 };
 
-const  DishDetails = (props) => {
-    const dish = props.dish;
-    if (dish) {
+const DishDetails = (props) => {
+    if (props.dish) {
         return (
             <div className="container">
                 <div className="row">
-                    {renderDish(dish)}
-                    {renderComments(dish.comments)}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
             </div>
         );
--- Coursera - Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ Coursera - Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
+import DishDetail from './DishDetails';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -32,12 +33,20 @@ class Main extends Component {
                 />
             );
         }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
                     <Redirect to="/home" />
                 </Switch>
