947e37c97c413f3bba0e20a5e2b2213696499cbc
Ankit Chaurasia <ac13793@gmail.com>
2018-09-13 22:07:33 +0530

    Testing AddExpensePage

--- Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/components/AddExpensePage.js
+++ Udemy - The Complete React Web Developer Course (with Redux)/expensify-app/src/components/AddExpensePage.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { addExpense } from "../actions/expenses";
-import ExpenseForm from "./ExpenseForm";
+import React from 'react';
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = props => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm
-      onSubmit={expense => {
-        console.log(expense);
-        props.dispatch(addExpense(expense));
-        props.history.push("/");
-      }}
-    />
-  </div>
-);
+export const AddExpensePage = props => {
+  const onSubmit = expense => {
+    props.onSubmit(expense);
+    props.history.push('/');
+  };
+  return (
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm onSubmit={onSubmit} />
+    </div>
+  );
+};
 
-export default connect()(AddExpensePage);
+const mapDispatchToProps = dispatch => ({
+  onSubmit: expense => dispatch(addExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
